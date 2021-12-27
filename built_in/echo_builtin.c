67e66edcc8ff4404f4fc6eaea062e4fc9ccd740d
Oglethorpe Garthar <ogarthar@oa-l3.msk.21-school.ru>
2021-12-27 18:25:41 +0300

    fixed echo

--- built_in/echo_builtin.c
+++ built_in/echo_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ogarthar <ogarthar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 19:28:40 by ogarthar          #+#    #+#             */
-/*   Updated: 2021/12/24 21:35:33 by ogarthar         ###   ########.fr       */
+/*   Updated: 2021/12/27 18:23:13 by ogarthar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,21 @@ int	ft_echo(t_arg *data)
 {
 	int	i;
 
+	i = 0;
 	if (!data->cmd->cmd[1])
 	{
-		printf("\n");
+		ft_putstr_fd("\n", 1);
 		return (0);
 	}
-	//*/*/*/*/*/*/*/*/*/*/*/*/*/*/
-	// if (!(ft_strcmp(data->cmd->cmd[1], "$?")))
-	// {
-	// 	free(data->cmd->cmd[1]);
-	// 	data->cmd->cmd[1] = ft_strdup(ft_itoa(data->errnum));
-	// }
-	//*/*/*/*/*/*/*/*/*/*/*/*/*/
-	i = 1;
 	if (!(ft_strcmp(data->cmd->cmd[1], "-n")))
-	{
-		while (data->cmd->cmd[++i])
-		{
-			printf("%s", data->cmd->cmd[i]);
-			if (data->cmd->cmd[i + 1] != NULL)
-				printf(" ");
-		}
-		return (0);
-	}
-	i = 0;
+		i = 1;
 	while (data->cmd->cmd[++i])
 	{
-		printf("%s", data->cmd->cmd[i]);
+		ft_putstr_fd(data->cmd->cmd[i], 1);
 		if (data->cmd->cmd[i + 1] != NULL)
-			printf(" ");
+			ft_putstr_fd(" ", 1);
 	}
-	printf("\n");
+	if (ft_strcmp(data->cmd->cmd[1], "-n"))
+		ft_putstr_fd("\n", 1);
 	return (0);
 }
